bc466f4a86c7ecd76e3036472af1e2d315630c1b
alim1381 <ali.moaydi2020@gmail.com>
2024-03-30 02:31:41 +0330

    test change for versel

--- src/modules/shop/shop.module.ts
+++ src/modules/shop/shop.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ShopService } from './shop.service';
 import { ShopController } from './shop.controller';
-import { DatabaseModule } from 'src/configs/database/database.module';
+import { DatabaseModule } from 'src/modules/database/database.module';
 import { shopProviders } from './providers/shop.provider';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [ShopController],
   providers: [...shopProviders, ShopService],
-  exports: [
-    ...shopProviders
-  ],
+  exports: [...shopProviders],
 })
 export class ShopModule {}
